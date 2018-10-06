2d0e70cd9a9ce2bf36d539683443fad96f6a7b4d
Newyorrker <newyorrker@gmail.com>
2018-10-06 18:27:19 +0300

    Codes data implementation

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,49 @@
 require('./libs.js');
 require('./callback.js');
 
+import $ from 'jquery';
+import select2 from 'select2';
 
 
 jQuery(document).ready(function($) {
 
 
+jQuery(document).ready(function($) {
 
 });
 
 document.addEventListener("DOMContentLoaded", function(event) {
+class Codes {
+	constructor() {
+    const selectData = [
+    	{
+    		dataUrl:'https://private-7b90d7-otc2.apiary-mock.com/codes/OKV',
+    		selector: '.OKV'
+    	},
+    	{
+    		dataUrl: 'https://private-7b90d7-otc2.apiary-mock.com/codes/OKP',
+    		selector: '.OKP'
+    	}
+    ];
+
+    this.getCodesMocks(selectData);
+  }
+  getCodesMocks(selectData) {
+  	selectData.forEach( function(element) {
+
+  		fetch(element.dataUrl).then(function(response) {
+				return response.json();
+			})
+			.then(function (data) {
+				$(element.selector).select2({
+					data: data,
+					placeholder: "Выберетие код"
+				})
+			});
+  	});
+  }
+}
+
+const selectCode = new Codes();
 
 });

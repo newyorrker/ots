6be3a32283f6ba53ed469131fb32dd23de25bd9f
Newyorrker <newyorrker@gmail.com>
2018-10-07 11:17:04 +0300

    PriceTypeChange class
    Комит с реализацией класса PriceTypeChange

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,3 @@
-require('./libs.js');
-// require('./callback.js');
-
 import $ from 'jquery';
 import select2 from 'select2';
 import jQueryScrollbar from 'jquery.scrollbar'
@@ -8,6 +5,83 @@ import jQueryScrollbar from 'jquery.scrollbar'
 
 jQuery(document).ready(function($) {
 
+class PriceTypeChange {
+	constructor() {
+
+		$('#price-not-mounted').prop('checked', false);
+		$('#for-unit').prop("checked");
+
+		this.eventsInit();
+	}
+
+	eventsInit() {
+
+		const radioBtn = $('.price-type input[type="radio"]');
+		const checkbox = $('.price-type input[type="checkbox"]');
+		const calculateControl = $('.price-calculate-control');
+
+
+		calculateControl.on('change', (e) => {
+			this.priceCalculate();
+		})
+
+		radioBtn.on('change', (e) => {
+			this.maxPriceToggle();
+		});
+
+		checkbox.on('change', (e) => {
+			this.priceToggle();
+		});
+	}
+	priceCalculate() {
+
+		const priceValue = $('#price-value').val();
+		const noNDSprice = $('#no-nds-price');
+		const ndsSum = $('#nds-sum');
+		const ndsRate = $('#nds-rate').val();
+
+
+		function ndsToNumber(ndsRate) {
+			if (ndsRate.includes('%')) {
+				return Number.parseInt(ndsRate.replace('%',''))
+			}
+			return false
+		}
+
+		function calcPrices (priceValue, ndsRateValue) {
+
+			if (!ndsRateValue) { 					//если поле не содержит символа процента
+				noNDSprice.val(priceValue); //то цена не изменяется, а сумма ндс равна нулю
+				ndsSum.val(0);
+				return true 								//завершаем выполнение функции
+			}
+
+			function noNDSpriceCalc() {
+				return priceValue - ( priceValue * ( ndsRateValue / 100 ));
+			}
+
+			function ndsSumCalc() {
+				return priceValue - noNDSpriceCalc();
+			}
+
+			noNDSprice.val( noNDSpriceCalc(priceValue, ndsRateValue));
+			ndsSum.val( ndsSumCalc(priceValue, noNDSpriceCalc()) );
+
+		}
+
+		calcPrices(priceValue, ndsToNumber(ndsRate))
+
+	}
+
+	maxPriceToggle() {
+		$('#price-not-mounted').parent().slideToggle();
+	}
+
+	priceToggle() {
+		const priceType = $('#price-not-mounted').closest('.price-type').next();
+		priceType.slideToggle();
+	}
+}
 
 $('.scrollbar-light').scrollbar();
 class Codes {
